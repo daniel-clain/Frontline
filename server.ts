1d5fbfa5fbb2ee243d4ecff981ac39a4718d8b54
Process-DanielC <65156182+Process-DanielC@users.noreply.github.com>
2021-07-01 15:50:50 +1000

    wip

--- server.ts
+++ server.ts
@@ -8,5 +8,8 @@ app.use(express.static('host-package'));
 app.get('/favicon.ico', (req, res) => {
   res.sendFile(`${__dirname}/favicon.ico`)
 })
+app.get('*', (req, res) => {
+  console.log(`yo dawg, we got a request from ${req.url}`);
+})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
