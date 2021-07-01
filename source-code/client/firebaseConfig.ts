556b5ff5675109c3daaa5e5b305e9e84ec083cb9
Process-DanielC <65156182+Process-DanielC@users.noreply.github.com>
2021-07-01 16:03:13 +1000

    asdf

---
+++ source-code/client/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfigSlut = {
+  apiKey: "AIzaSyDC6JbspPbhZMuV6aTuOj_I5yAlmnvTBF4",
+  authDomain: "frontline-89c8f.firebaseapp.com",
+  projectId: "frontline-89c8f",
+  storageBucket: "frontline-89c8f.appspot.com",
+  messagingSenderId: "646305983944",
+  appId: "1:646305983944:web:728dc241395692d70524fd"
+};
--- source-code/client/index.tsx
+++ source-code/client/index.tsx
@@ -3,9 +3,9 @@ import { render } from 'react-dom'
 import * as React from 'react';
 import {Suspense, lazy} from 'react';
 import firebase from 'firebase/app'
-import {firebaseConfig} from './firebase.config';
+import {firebaseConfigSlut} from './firebaseConfig';
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfigSlut)
 const reactRenderingTag = document.createElement('react')
 document.body.appendChild(reactRenderingTag)
 
